0232518717a3900b7029d2b9fa3e66930a5d28dd
Sooraj Bhooshan <sooraj.bhooshan@waltr.in>
2023-12-22 18:19:29 +0530

    completed  till event forwarding

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import ElevatedButton from "./ElevatedButton.svelte";
+
 	let name = "Stupid";
 	let bgColor = "red";
 	let firstName = "Jack";
@@ -17,21 +19,64 @@
 	const handleInput = (e) => {
 		bgColor = e.target.value;
 	};
+
+	let people = [
+		{ name: "Manu", age: 20, skill: "CSS", id: 1 },
+		{ name: "Raj", age: 25, skill: "JavaScript", id: 2 },
+		{ name: "Kannan", age: 18, skill: "Dart", id: 3 },
+		{ name: "Jazz", age: 6, skill: "Flutter", id: 4 },
+	];
+
+	const removePerson = (id) => {
+		people = people.filter((person) => person.id != id);
+	};
+	let showModal = false;
+
+	const toggleModel = () => {
+		showModal = !showModal;
+	};
 </script>
 
 <main>
 	<h1 style="color: {bgColor};">Hello {bgColor}!</h1>
 	<button on:click={changeColor}>Change color</button>
-	<!-- both does the same thing -->
-	<!-- on input it will be one way binding, add value and then it will be to way binding -->
-	<!-- <input type="text" on:input={handleInput} value={bgColor} /> -->
+
+	both does the same thing on input it will be one way binding, add value and
+	then it will be to way binding
+	<input type="text" on:input={handleInput} value={bgColor} />
 	<input type="text" bind:value={bgColor} />
 	<input type="text" bind:value={firstName} />
 	<input type="text" bind:value={lastName} />
 	<p>{fullName}</p>
+
+	<ElevatedButton text="Life is Hard" {showModal} on:click={toggleModel} />
+
+	{#each people as person (person.id)}
+		<div class="info">
+			{#if person.skill === "CSS"}
+				<p>I am a Pro</p>
+			{/if}
+			<h3>{person.name}</h3>
+			<p>{person.age}</p>
+			<button
+				on:click={() => {
+					removePerson(person.id);
+				}}>Delete</button
+			>
+		</div>
+	{:else}
+		<p>So sorry,,,, nobodys here</p>
+	{/each}
+
+	<button on:click={toggleModel}>Toggle</button>
 </main>
 
 <style>
+	.info {
+		background-color: lightblue;
+		padding: 20px;
+		margin: 20px;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
---
+++ src/ElevatedButton.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let showModal = false;
+    let isPromo = false;
+    export let text = "It's me the default text";
+</script>
+
+{#if showModal}
+    <div class="backdrop" on:click>
+        <div class="modal" class:promo={isPromo}>{text}</div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background-color: rgba(0, 0, 0, 0.8);
+    }
+
+    .modal {
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        text-align: center;
+        margin: 10% auto;
+        background: white;
+    }
+    .promo {
+        background: red;
+        color: white;
+    }
+</style>
